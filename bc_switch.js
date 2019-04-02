4b25785c1f4574d89c6f35ca1144d28db3f3a9b7
Alex Somoza <ASomoza041@west-mec.org>
2019-04-02 16:53:55 -0700

    Final Commit
    Finished setting up buttons to change the look of the page. Ready for review.

--- bc_switch.js
+++ bc_switch.js
@@ -26,11 +26,11 @@ function setupStyles() {
     pageStyle.setAttribute("rel", "stylesheet");
     pageStyle.setAttribute("disabled", "disabled");
 
-    //append the link element to the document head
+    //append the link element tot eh document head
     document.head.appendChild(pageStyle);
     pageStyle.disabled = true;
 
-    //insert buttons for the style switcher
+    //insert buttond fot the style switcher
     var buttonDIV = document.createElement("div");
     buttonDIV.setAttribute("id", "styleButtons");
 
@@ -47,13 +47,43 @@ function setupStyles() {
 
     document.body.insertBefore(buttonDIV, document.body.firstChild);
 
-    //append an embedded stylesheet to the document head
-    var buttonStyles = document.createElement("stlye");
+    //append an embbeded stylesheet to the document head
+    var buttonStyles = document.createElement("style");
     document.head.appendChild(buttonStyles);
 
-    //add style rules to the embedded style sheet
+    //add style rules to the document
     document.styleSheets[document.styleSheets.length - 1].insertRule(
         "div#styleButtons { \
             position: fixed; \
         }", 0);
+
+    document.styleSheets[document.styleSheets.length - 1].insertRule(
+        "div#styleButtons input { \
+                  background-color: rgba(68, 94, 186, 0.6); \
+                  border: 3px solid rgba(0, 24, 123, 0.6); \
+                  border-radius: 50%; \
+                  cursor: pointer; \
+                  color: white; \
+                  display: inline-block; \
+                  font-size: 1.2em; \
+                  height: 60px; \
+                  margin: 5px 10px; \
+                  width: 100px; \
+               }", 1);
+
+    document.styleSheets[document.styleSheets.length - 1].insertRule(
+        "@media print { \
+                     div#styleButtons { \
+                        display: none; \
+                     } \
+                  }", 2);
+
+    //turn the page view style off and on
+    webButton.onclick = function () {
+        pageStyle.disabled = true;
+    }
+
+    pageButton.onclick = function () {
+        pageStyle.disabled = false;
+    }
 }
